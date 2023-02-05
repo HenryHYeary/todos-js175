d8aaa73f62e3a92fc6f2814eb6b4543b6f761459
Henry Yeary <henmofb@gmail.com>
2023-02-04 22:03:46 -0600

    Added two missing todos and sorting for lists.

--- todos.js
+++ todos.js
@@ -5,6 +5,16 @@ const app = express();
 const host = "localhost";
 const port = 3000;
 
+const compareByTitle = (todoListA, todoListB) => {
+  if (todoListA.title < todoListB.title) {
+    return -1;
+  } else if (todoListA.title > todoListB.title) {
+    return 1;
+  } else {
+    return 0
+  }
+};
+
 let todoLists = require('./lib/seed-data');
 
 app.set("views", "./views");
@@ -13,8 +23,20 @@ app.set("view engine", "pug");
 app.use(morgan("common"));
 app.use(express.static('public'));
 
+const sortTodoLists = lists => {
+  let undoneLists = lists.filter(todoList => !todoList.isDone());
+  let doneLists = lists.filter(todoList => todoList.isDone());
+
+  let sortedUndoneList = undoneLists.sort(compareByTitle);
+  let sortedDoneList = doneLists.sort(compareByTitle);
+
+  return sortedUndoneList.concat(sortedDoneList);
+};
+
 app.get("/", (req, res) => {
-  res.render("lists", { todoLists });
+  res.render("lists", { 
+    todoLists: sortTodoLists(todoLists), 
+  });
 });
 
 app.listen(port, host, () => {
