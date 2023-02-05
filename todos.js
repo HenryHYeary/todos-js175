04bbbe1481820ce1f7cfc22ced341f08e1a8142e
Henry Yeary <henmofb@gmail.com>
2023-02-05 11:08:00 -0600

    Added error handling capabilities.

--- todos.js
+++ todos.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const morgan = require("morgan");
+const TodoList = require('./lib/todolist');
 
 const app = express();
 const host = "localhost";
 const port = 3000;
 
 const compareByTitle = (todoListA, todoListB) => {
-  if (todoListA.title < todoListB.title) {
+  let titleA = todoListA.title.toLowerCase();
+  let titleB = todoListB.title.toLowerCase();
+
+  if (titleA < titleB) {
     return -1;
-  } else if (todoListA.title > todoListB.title) {
+  } else if (titleA > titleB) {
     return 1;
   } else {
     return 0
@@ -22,6 +26,7 @@ app.set("view engine", "pug");
 
 app.use(morgan("common"));
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
 
 const sortTodoLists = lists => {
   let undoneLists = lists.filter(todoList => !todoList.isDone());
@@ -34,9 +39,40 @@ const sortTodoLists = lists => {
 };
 
 app.get("/", (req, res) => {
-  res.render("lists", { 
-    todoLists: sortTodoLists(todoLists), 
+  res.redirect("/lists");
+});
+
+app.get("/lists", (req, res) => {
+  res.render("lists", {
+    todoLists: sortTodoLists(todoLists),
   });
+});
+
+app.get("/lists/new", (req, res) => {
+  res.render("new-list");
+});
+
+app.post("/lists", (req, res) => {
+  let title = req.body.todoListTitle.trim();
+  if (title.length === 0) {
+    res.render("new-list", {
+      errorMessage: "A title was not provided.",
+    });
+  } else if (title.length > 100) {
+    res.render("new-list", {
+      errorMessage: "List title must be between 1 and 100 characters.",
+      todoListTitle: title,
+    });
+
+  } else if (todoLists.some(list => list.title === title)) {
+    res.render("new-list", {
+      errorMessage: "List title must be unique.",
+      todoListTitle: title,
+    });
+  } else {
+    todoLists.push(new TodoList(title));
+    res.redirect("/lists");
+  }
 });
 
 app.listen(port, host, () => {
